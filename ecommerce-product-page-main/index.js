11b028967959958b452c6925e14d8dc17aa99bc7
dumeccs <88063009+dumeccs@users.noreply.github.com>
2022-05-05 18:40:57 +0100

    add the display item number on the checkout feature

--- ecommerce-product-page-main/index.js
+++ ecommerce-product-page-main/index.js
@@ -4,23 +4,32 @@ const closeBtn = document.getElementsByClassName('icon-close')[0]
 const container = document.getElementsByClassName('container')[0]
 const checkoutBtn = document.getElementById('checkout-basket')
 const checkoutCart = document.getElementsByClassName('cart-checkout')[0]
-console.log(navList)
+const addItemBtn = document.getElementsByClassName('icon-plus')[0]
+const minusItemBtn = document.getElementsByClassName('icon-minus')[0]
+let amountInNumber = document.getElementsByClassName('display-amount')[0]
+const addToCart = document.getElementById('add--to--cart')
+const inCart = document.getElementById('in-cart')
+let itemNumber = 0
+
+barIcon.addEventListener('click',onToggle)
+closeBtn.addEventListener('click', closeSideNav)
+checkoutBtn.addEventListener('click', checkoutDisplay)
+addItemBtn.addEventListener('click',addItem)
+minusItemBtn.addEventListener('click',removeItem)
+
+
 function onToggle(){
     navList.classList.add('active')
     closeBtn.style.display ='block'
-    container.classList.add('fade-bg')
     checkoutCart.style.display = 'none'
-    
-   
 }
-closeBtn.addEventListener('click', function(){
+
+function closeSideNav(){
     navList.classList.remove('active')
     closeBtn.style.display ="none"
+}
 
-} )
-barIcon.addEventListener('click',onToggle)
-
-checkoutBtn.addEventListener('click', function(){
+function checkoutDisplay(){
     if(checkoutCart.style.display === 'block'){
         checkoutCart.style.display = 'none'
     }
@@ -29,4 +38,32 @@ checkoutBtn.addEventListener('click', function(){
     }
     navList.classList.remove('active')
     closeBtn.style.display ="none"
-})
+}
+
+function setAmountInNumber(){
+    amountInNumber.textContent = amountInNumber
+}
+
+function addItem(){
+        itemNumber++
+        amountInNumber.textContent = itemNumber
+}
+
+function removeItem(){
+    if (itemNumber > 0){
+        itemNumber--
+    }
+    amountInNumber.textContent = itemNumber
+}
+
+
+
+function displayItemsInCart(){
+    inCart.textContent = itemNumber
+    
+}
+addToCart.addEventListener('click',displayItemsInCart)
+
+
+
+
